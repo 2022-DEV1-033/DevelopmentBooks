ae399ade6b885dde51ab60f8d4693483a264562b
gholizadeh <gholizade.saeed@yahoo.com>
2022-06-25 14:23:05 +0430

    BNPP market rule refactor

--- src/main/java/com/bnpp/katas/developmentbooks/service/market/BNPPMarketRule.java
+++ src/main/java/com/bnpp/katas/developmentbooks/service/market/BNPPMarketRule.java
@@ -18,39 +18,50 @@ public class BNPPMarketRule implements MarketRule{
     @Value("#{${app.market.bnpp.discounts}}")
     private HashMap<Integer,Integer> bnppDiscounts;
 
+    private BigDecimal discount = BigDecimal.ZERO;
+
     @Override
     public BigDecimal calculateDiscount(Map<Long, Integer> itemsMap, List<Book> books) {
-        Map<Long, Integer> itemsMapToCalculate = new HashMap<>();
-        itemsMapToCalculate.putAll(itemsMap);
+        Map<Long, Integer> itemsMapToCalculate = cloneItems(itemsMap);
         Integer maxPackSize = bnppDiscounts.entrySet().stream().map(Map.Entry::getKey).max(Integer::compareTo).get();
         Map<Long, Book> booksMap = books.stream().collect(Collectors.toMap(Book::getId, book -> book));
-        BigDecimal discount = BigDecimal.ZERO;
 
         while (itemsMapToCalculate.size() > 0) {
-            int packSize = 0;
-            BigDecimal packPrice = BigDecimal.ZERO;
-
-            for (Iterator<Map.Entry<Long, Integer>> iterator = itemsMapToCalculate.entrySet().iterator();
-                 iterator.hasNext() && packSize < maxPackSize; ) {
-                Map.Entry<Long, Integer> entry = iterator.next();
-                if(entry.getValue() > 0) {
-                    packPrice = packPrice.add(booksMap.get(entry.getKey()).getPrice());
-                    packSize++;
-                    entry.setValue(entry.getValue() - 1);
-                }
-
-                if(entry.getValue() == 0) {
-                    iterator.remove();
-                }
-            }
-
-            if(bnppDiscounts.containsKey(packSize)){
-                BigDecimal packDiscount = packPrice.multiply(new BigDecimal(bnppDiscounts.get(packSize)))
-                        .divide(new BigDecimal(100), 2, RoundingMode.FLOOR);
-                discount = discount.add(packDiscount);
-            }
+            createPack(itemsMapToCalculate, maxPackSize, booksMap);
         }
 
         return discount;
+    }
+
+    private BigDecimal createPack(Map<Long, Integer> itemsMapToCalculate, Integer maxPackSize, Map<Long, Book> booksMap) {
+        int packSize = 0;
+        BigDecimal packPrice = BigDecimal.ZERO;
+
+        for (Iterator<Map.Entry<Long, Integer>> iterator = itemsMapToCalculate.entrySet().iterator();
+             iterator.hasNext() && packSize < maxPackSize; ) {
+            Map.Entry<Long, Integer> entry = iterator.next();
+            if(entry.getValue() > 0) {
+                packPrice = packPrice.add(booksMap.get(entry.getKey()).getPrice());
+                packSize++;
+                entry.setValue(entry.getValue() - 1);
+            }
+
+            if(entry.getValue() == 0) {
+                iterator.remove();
+            }
+        }
+
+        if(bnppDiscounts.containsKey(packSize)){
+            BigDecimal packDiscount = packPrice.multiply(new BigDecimal(bnppDiscounts.get(packSize)))
+                    .divide(new BigDecimal(100), 2, RoundingMode.FLOOR);
+            discount = discount.add(packDiscount);
+        }
+        return discount;
+    }
+
+    private Map<Long, Integer> cloneItems(Map<Long, Integer> itemsMap) {
+        Map<Long, Integer> itemsClone = new HashMap<>();
+        itemsClone.putAll(itemsMap);
+        return itemsClone;
     }
 }
